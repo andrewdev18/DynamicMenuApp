34cb93385a048e012c8259508e2d6b4fb3229772
andrewdev18 <jaime.mora2018@uteq.edu.ec>
2022-01-16 21:10:04 -0500

    Validación del nombre en menu

--- app/src/main/java/com/example/dynamicmenuapp/IndexActivity.java
+++ app/src/main/java/com/example/dynamicmenuapp/IndexActivity.java
@@ -1,23 +1,63 @@
 package com.example.dynamicmenuapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
-public class IndexActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
-    TextView txtTest;
+import org.w3c.dom.Text;
+
+public class IndexActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    TextView lblUsername;
+    DrawerLayout drwLayout;
+    NavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
 
-        //txtTest = findViewById(R.id.lblTest);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.icon_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+
+        View header = navView.getHeaderView(0);
+        lblUsername = (TextView) header.findViewById(R.id.lblUsername);
         Bundle bundle = getIntent().getExtras();
         if(bundle != null){
-            //txtTest.setText(bundle.getString("user"));
+            String name = bundle.getString("user");
+            lblUsername.setText(name);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drwLayout = (DrawerLayout) findViewById(R.id.mainDrawerLayout);
+                drwLayout.openDrawer(GravityCompat.START);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
